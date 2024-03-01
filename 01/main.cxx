ac32d6067616715c3ea304c76d2f2abbb145ea28
Pranav Taysheti <pranavtaysheti@gmail.com>
2024-03-01 20:26:13 +0530

    imroved modularity

--- 01/main.cxx
+++ 01/main.cxx
@@ -3,7 +3,6 @@
 #include <string_view>
 #include <array>
 #include <vector>
-#include <utility>
 #include <algorithm>
 
 constexpr int ASCII_0 = 48;
@@ -20,11 +19,11 @@ const auto DIGIT_NAMES = std::array<std::string, 10>{
     "nine",
 };
 
-std::pair<int, int> parseStringNum(const std::string_view s, int pos)
+int parseStringNum(const std::string_view s, int pos)
 {
     pos += 1;
-    int i = 0;
 
+    int i = 0;
     for (std::string n : DIGIT_NAMES)
     {
         int begin = std::max(pos - static_cast<int>(n.length()), 0);
@@ -33,59 +32,75 @@ std::pair<int, int> parseStringNum(const std::string_view s, int pos)
         auto sub = s.substr(begin, len);
 
         if (sub == n)
-        {
-            return {i, 0};
-        }
+            return i;
 
         i += 1;
     }
 
-    return {0, 1};
+    return -1;
 }
 
-std::pair<int, int> parseDigit(const char c)
+int parseDigit(const char c)
 {
     if (c >= '0' and c <= '9')
+        return c - ASCII_0;
+
+    return -1;
+}
+
+std::vector<int> calculateP1(std::string_view s)
+{
+    std::vector<int> result = {};
+
+    for (const auto c : s)
     {
-        return {c - ASCII_0, 0};
+        auto n = parseDigit(c);
+        if (n >= 0)
+            result.push_back(n);
     }
 
-    return {0, 1};
+    return result;
+}
+
+std::vector<int> calculateP2(std::string_view s)
+{
+    std::vector<int> result = {};
+
+    for (int i = 0; i < s.length(); i += 1)
+    {
+        auto n = parseDigit(s[i]);
+        if (n >= 0)
+        {
+            result.push_back(n);
+            continue;
+        }
+
+        n = parseStringNum(s, i);
+        if (n >= 0)
+            result.push_back(n);
+    }
+
+    return result;
+}
+
+int solveNumVector(const std::vector<int> &vn)
+{
+    return 10 * vn.at(0) + vn.back();
 }
 
 int main(int argc, char const *argv[])
 {
     std::string l{};
-    int total_sum = 0;
+    int total_p1_sum = 0;
+    int total_p2_sum = 0;
 
     while (std::getline(std::cin, l))
     {
-        auto l_digits = std::vector<int>{};
-        int l_sum = 0;
-
-        for (int i = 0; i < l.length(); i += 1)
-        {
-            const auto [pd_r, pd_err] = parseDigit(l[i]);
-            if (pd_err == 0)
-            {
-                l_digits.push_back(pd_r);
-                continue;
-            }
-
-            const auto [psn_r, psn_err] = parseStringNum(l, i);
-            if (psn_err == 0)
-            {
-                l_digits.push_back(psn_r);
-                continue;
-            }
-        }
-
-        l_sum += l_digits[0];
-        l_sum *= 10;
-        l_sum += l_digits[l_digits.size() - 1];
-        total_sum += l_sum;
+        total_p1_sum += solveNumVector(calculateP1(l));
+        total_p2_sum += solveNumVector(calculateP2(l));
     }
 
-    std::cout << total_sum << '\n';
+    std::cout << "P1: " << total_p1_sum << '\n';
+    std::cout << "P2: " << total_p2_sum << '\n';
     return 0;
 }
