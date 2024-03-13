78bea9301a6c74ca00f61c3ad85c437be8a56c5c
Pranav Taysheti <pranavtaysheti@gmail.com>
2024-03-13 10:08:31 +0530

    optimization and cleanup

--- 05/main.py
+++ 05/main.py
@@ -23,24 +23,11 @@ class MapItemRange(NamedTuple):
 type Range = tuple[int, int]
 
 type MasterMap = list[list[MapItem]]
-_master_map: MasterMap = []
+master_map: MasterMap = []
 
 
-def add_layer_mastermap() -> None:
-    _master_map.append([])
-
-    if len(_master_map) == 1:
-        return
-
-    _master_map[-2].sort(key=lambda mi: mi.of_start)
-
-
-def add_item_mastermap(mil: MapItem):
-    _master_map[-1].append(mil)
-
-
-def get_mastermap() -> MasterMap:
-    return _master_map
+def sort_layer(layer: list[MapItem]) -> None:
+    layer.sort(key=lambda mi: mi.of_start)
 
 
 def calc_max(n: tuple[int, int]) -> int:
@@ -70,23 +57,22 @@ def mil_next_value(v: int, next_map: list[MapItem]) -> int:
 
 def solve_detached(seed: int):
     s = seed
-    for next_mapping in get_mastermap():
+    for next_mapping in master_map:
         s = mil_next_value(s, next_mapping)
 
     return s
 
 
-def mi_next_range(pr: Range, mi: MapItem) -> Range:
+def _mi_next_range(pr: Range, mi: MapItem) -> Range:
     p_min, p_max = pr
     mi_of_min, mi_of_max = mi_range(mi).of_range
-    c_min, c_max = max(p_min, mi_of_min), min(p_max, mi_of_max)
 
-    if (n_min := mi_next_value(c_min, mi)) is not None and (
-        n_max := mi_next_value(c_max, mi)
+    if (n_min := mi_next_value(p_min, mi)) is not None and (
+        n_max := mi_next_value(p_max, mi)
     ) is not None:
         return n_min, n_max
     else:
-        raise Exception("mi_next_value returned None. Should not happen!!")
+        raise Exception("mi_next_value returned None.")
 
 
 def _mil_next_ranges(p: Range, mil: list[MapItem]) -> list[Range]:
@@ -96,16 +82,14 @@ def _mil_next_ranges(p: Range, mil: list[MapItem]) -> list[Range]:
     for mi in mil:
         mi_of_min, mi_of_max = mi_range(mi).of_range
 
-        if mi_of_min > p_max:  # PROBLEM
+        if mi_of_min > p_max:
             break
 
         if (c_min := max(p_min, mi_of_min)) < (c_max := min(p_max, mi_of_max)):
             if c_min > p_min:
                 result.append((p_min, c_min - 1))
-            if (n_range := mi_next_range((c_min, c_max), mi)) is not None:
-                result.append(n_range)
-            else:
-                raise Exception("RETURNED NONE! should not happen!!!")
+
+            result.append(_mi_next_range((c_min, c_max), mi))
             p_min = c_max + 1
 
     if p_min < p_max:
@@ -114,11 +98,11 @@ def _mil_next_ranges(p: Range, mil: list[MapItem]) -> list[Range]:
     return result
 
 
-def solve_range(n: Range) -> list[Range]:
-    curr_ranges: list[Range] = [n]
+def solve_range(rl: list[Range]) -> list[Range]:
+    curr_ranges: list[Range] = rl
     next_ranges: list[Range] = []
 
-    for i, layer in enumerate(get_mastermap()):
+    for i, layer in enumerate(master_map):
         for r in curr_ranges:
             next_ranges.extend(_mil_next_ranges(r, layer))
         curr_ranges = next_ranges
@@ -127,32 +111,30 @@ def solve_range(n: Range) -> list[Range]:
     return curr_ranges
 
 
-if __name__ == "__main__":
-    with open("input.txt", "r") as input_file:
-        seeds = [int(s) for s in next(input_file)[7:].split()]
+with open("input.txt", "r") as input_file:
+    seeds = [int(s) for s in next(input_file)[7:].split()]
 
-        for l in input_file:
-            if len(l) <= 1:
-                continue
+    for l in input_file:
+        if len(l) <= 1:
+            continue
 
-            if l[-2] == ":":
-                add_layer_mastermap()
-                continue
+        if l[-2] == ":":
+            if len(master_map) > 0:
+                sort_layer(master_map[-1])
 
-            add_item_mastermap(MapItem(*(int(n) for n in l.split())))
+            master_map.append([])
+            continue
 
-    add_layer_mastermap()
-    get_mastermap().pop()
+        master_map[-1].append(MapItem(*(int(n) for n in l.split())))
 
-    p1_sol = min(solve_detached(s) for s in seeds)
+    sort_layer(master_map[-1])
 
-    seeds_ranged = ((low, calc_max((low, range_))) for low, range_ in batched(seeds, 2))
-    seeds_ranged_result: list[int] = []
-    for seed_low, seed_high in seeds_ranged:
-        seeds_ranged_result.append(
-            min(r[0] for r in solve_range((seed_low, seed_high)))
-        )
-    p2_sol = min(seeds_ranged_result)
+p1_sol = min(solve_detached(s) for s in seeds)
 
-    print(f"P1: {p1_sol}")
-    print(f"P2: {p2_sol}")
+seeds_ranged = ((low, calc_max((low, range_))) for low, range_ in batched(seeds, 2))
+seeds_ranged_result: list[int] = []
+seeds_ranged_result.append(min(r[0] for r in solve_range([*seeds_ranged])))
+p2_sol = min(seeds_ranged_result)
+
+print(f"P1: {p1_sol}")
+print(f"P2: {p2_sol}")
