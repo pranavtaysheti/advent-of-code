298b86826c42c3b9cfb09482543cfde67301b03a
pranavtaysheti <pranavtaysheti@gmail.com>
2024-06-03 00:45:48 +0530

    D08 P02

--- 08/main.py
+++ 08/main.py
@@ -1,16 +1,24 @@
 from __future__ import annotations
 
 import fileinput
+import math
+import re
+from collections import OrderedDict
+from collections.abc import Iterable
 from enum import StrEnum
-from itertools import cycle
-from turtle import right
+from itertools import pairwise
 from typing import NamedTuple
 
-instructions: list[Direction] = []
-network: dict[str, NodeNavigation] = {}
 
-P1: int = 0
-P2: int = 0
+class NodeNavigation(NamedTuple):
+    left: str
+    right: str
+
+
+class NavigatorResult(NamedTuple):
+    end: str
+    steps: int
+    pos: int
 
 
 class Direction(StrEnum):
@@ -18,9 +26,8 @@ class Direction(StrEnum):
     LEFT = "L"
 
 
-class NodeNavigation(NamedTuple):
-    left: str
-    right: str
+instructions: list[Direction] = []
+network: OrderedDict[str, NodeNavigation] = OrderedDict()
 
 
 def parse_input(input_file: fileinput.FileInput[str]):
@@ -36,32 +43,62 @@ def parse_input(input_file: fileinput.FileInput[str]):
         network[node] = NodeNavigation(left, right)
 
 
-def navigate(init: str, end: str) -> int:
-    count: int = 0
+def navigate(init: str, end_pattern: str, instruction_pos: int = 0) -> NavigatorResult:
+    steps: int = 0
     next = init
+    curr: int = instruction_pos
 
-    for step in cycle(instructions):
-        count += 1
+    while re.search(end_pattern, next) is None:
+        steps += 1
 
-        match step:
+        match instructions[curr]:
             case Direction.LEFT:
                 next = network[next].left
             case Direction.RIGHT:
                 next = network[next].right
 
-        if next == end:
-            break
+        curr = (curr + 1) % len(instructions)
 
-    return count
+    return NavigatorResult(next, steps, curr)
+
+
+def navigate_thorough(init: str, end_pattern: str):
+    steps_db: list[int] = []
+    next = init
+    end_pos = None
+
+    while end_pos != 0:
+        end, steps, end_pos = navigate(next, end_pattern)
+        steps_db.append(steps)
+        next = end
+
+    return steps_db
+
+
+def navigate_all(init_pattern: str, end_pattern: str) -> dict[str, list[int]]:
+    routes: dict[str, list[int]] = {}
+
+    for node in network:
+        if re.search(init_pattern, node) is None:
+            continue
+
+        routes[node] = navigate_thorough(node, end_pattern)
+
+    return routes
+
+
+def lcm(numbers: Iterable[int]):
+    return math.prod(numbers) // math.prod(
+        [math.gcd(x, y) for x, y in pairwise(numbers)]
+    )
 
 
 def main():
-    global P1, P2
-
     with fileinput.input(encoding="utf-8") as input_file:
         parse_input(input_file)
 
-    P1 = navigate("AAA", "ZZZ")
+    _, P1, _ = navigate("AAA", "^ZZZ$")
+    P2: int = lcm([sum(n) for n in navigate_all("A$", "Z$").values()])
 
     print(f"P1: {P1}")
     print(f"P2: {P2}")
