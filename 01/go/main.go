44e40ddc024956bec3f8b6bc1d649c3e13b1bd46
pranavtaysheti <pranavtaysheti@gmail.com>
2024-07-13 13:59:08 +0530

    Go D01 P1&2

--- 01/go/main.go
+++ 01/go/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 var numberStrings = []string{
@@ -33,27 +34,52 @@ func parseInput(r io.Reader) {
 	}
 }
 
-func extractDigits(l string) (r []int) {
-	for _, c := range l {
+func extractNumbers(l string, countSpelled bool) (r []int) {
+	getDigit := func(c rune) bool {
 		if int(c) >= int('0') && int(c) <= int('9') {
 			r = append(r, int(c)-int('0'))
+			return true
+		}
+
+		return false
+	}
+
+	getSpelled := func(s string) bool {
+		for i, sub := range numberStrings {
+			if strings.Contains(s[max(0, len(s)-len(sub)):], sub) {
+				r = append(r, i)
+				return true
+			}
+
+		}
+
+		return false
+	}
+
+	for i, c := range l {
+		if getDigit(c) {
+			continue
+		}
+
+		if countSpelled {
+			getSpelled(l[max(0, i-4) : i+1])
 		}
 	}
 
 	return
 }
 
-func decodeDigits(d []int) int {
-	if len(d) == 0 {
-		return 0
+func sumNumbers(countSpelled bool) (r int) {
+	decodeDigits := func(d []int) int {
+		if len(d) == 0 {
+			return 0
+		}
+
+		return 10*d[0] + d[len(d)-1]
 	}
 
-	return 10*d[0] + d[len(d)-1]
-}
-
-func sumNumbers() (r int) {
 	for _, l := range data {
-		r += decodeDigits(extractDigits(l))
+		r += decodeDigits(extractNumbers(l, countSpelled))
 	}
 
 	return
@@ -61,8 +87,8 @@ func sumNumbers() (r int) {
 func main() {
 	parseInput(os.Stdin)
 
-	P1 := sumNumbers()
-	P2 := 0
+	P1 := sumNumbers(false)
+	P2 := sumNumbers(true)
 
 	fmt.Printf("P1: %d \n", P1)
 	fmt.Printf("P2: %d \n", P2)
