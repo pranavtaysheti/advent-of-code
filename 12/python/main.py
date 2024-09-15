75740ea72b2f2f3aab1adfbaf7b17e8eabba94a6
pranavtaysheti <pranavtaysheti@gmail.com>
2024-09-16 00:24:25 +0530

    P1&2

--- 12/python/main.py
+++ 12/python/main.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import fileinput
 from enum import StrEnum
+from functools import cache
 from typing import NamedTuple
 
 
@@ -10,11 +13,65 @@ class Spring(StrEnum):
 
 
 class Record(NamedTuple):
-    springs: list[Spring]
-    counts: list[int]
+    springs: tuple[Spring, ...]
+    counts: tuple[int, ...]
+
+    def solve(self) -> int:
+        return self._solve(self.springs, self.counts, False)
+
+    @cache
+    def _solve(
+        self, springs: tuple[Spring, ...], counts: tuple[int, ...], active: bool
+    ) -> int:
+        if len(springs) == 0 and (len(counts) > 0 and counts != (0,)):
+            return 0
+
+        if len(springs) == 0 and (len(counts) == 0 or counts == (0,)):
+            return 1
+
+        new_counts: list[int] = list(counts)
+        match springs[0]:
+
+            case Spring.Unknown:
+                r1 = self._solve(
+                    (Spring.Damaged, *springs[1:]), tuple(new_counts), active
+                )
+                r2 = self._solve(
+                    (Spring.Operational, *springs[1:]), tuple(new_counts), active
+                )
+                return r1 + r2
+
+            case Spring.Operational:
+                if active:
+                    if new_counts[0] > 0:
+                        return 0
+
+                    if new_counts[0] == 0:
+                        active = False
+                        new_counts = new_counts[1:]
+            case Spring.Damaged:
+                if len(new_counts) == 0:
+                    return 0
+
+                if new_counts[0] == 0 and active:
+                    return 0
+
+                if new_counts[0] == 0 and not active:
+                    print("wird thing happened")
+                new_counts[0] -= 1
+                active = True
+
+        return self._solve(springs[1:], tuple(new_counts), active)
 
 
 data: list[Record]
+
+
+def expand_record(record: Record):
+    return Record(
+        springs=((*record.springs, Spring.Unknown) * 5)[:-1],
+        counts=record.counts * 5,
+    )
 
 
 def parse_input(input: fileinput.FileInput[str]):
@@ -24,8 +81,8 @@ def parse_input(input: fileinput.FileInput[str]):
         springs_str, counts_str = line.split()
         res.append(
             Record(
-                springs=[Spring(c) for c in springs_str],
-                counts=[int(n) for n in counts_str.split(",")],
+                springs=tuple(Spring(c) for c in springs_str),
+                counts=tuple(int(n) for n in counts_str.split(",")),
             )
         )
 
@@ -38,7 +95,11 @@ def main():
     with fileinput.input(encoding="utf-8") as input_file:
         data = parse_input(input_file)
 
-    print(data)
+    P1 = sum(r.solve() for r in data)
+    P2 = sum(expand_record(r).solve() for r in data)
+
+    print(f"P1: {P1}")
+    print(f"P2: {P2}")
 
 
 if __name__ == "__main__":
