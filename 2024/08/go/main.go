9e0c3bc9e32fbd1cf2c87b03969bfa198330f1ef
Pranav Taysheti <pranavtaysheti@gmail.com>
2024-12-08 16:32:48 +0530

    P2

--- 2024/08/go/main.go
+++ 2024/08/go/main.go
@@ -26,20 +26,35 @@ func (a antennaMap) combinations() iter.Seq[[2][2]int] {
 	}
 }
 
-func (a antennaMap) mapAntinodes() map[[2]int]struct{} {
-	res := map[[2]int]struct{}{}
+func (a antennaMap) mapAntinodes() map[[2]int]bool {
+	res := map[[2]int]bool{}
 
 	for posPair := range a.combinations() {
 		pos1, pos2 := posPair[0], posPair[1]
 		cRow, cCol := pos2[0]-pos1[0], pos2[1]-pos1[1]
 
-		antinodes := [2][2]int{
-			{pos2[0] + cRow, pos2[1] + cCol},
-			{pos1[0] - cRow, pos1[1] - cCol}}
+		for i := 0; ; i++ {
+			antinodes := [2][2]int{
+				{pos2[0] + i*cRow, pos2[1] + i*cCol},
+				{pos1[0] - i*cRow, pos1[1] - i*cCol}}
 
-		for _, an := range antinodes {
-			if an[0] >= 0 && an[0] < size && an[1] >= 0 && an[1] < size {
-				res[an] = struct{}{}
+			rejected := 0
+			for _, pos := range antinodes {
+				if pos[0] >= 0 && pos[0] < size && pos[1] >= 0 && pos[1] < size {
+					if i == 1 {
+						res[pos] = true
+					} else {
+						if _, ok := res[pos]; !ok {
+							res[pos] = false
+						}
+					}
+				} else {
+					rejected++
+				}
+			}
+
+			if rejected == 2 {
+				break
 			}
 		}
 	}
@@ -68,19 +83,28 @@ func parse(r io.Reader) {
 			a[[2]int{i, j}] = struct{}{}
 		}
 
-		size = i+1
+		size = i + 1
 	}
 }
 
 func main() {
 	parse(os.Stdin)
+
 	antinodeMap := map[[2]int]struct{}{}
+	firstHarmonic := map[[2]int]struct{}{}
+
 	for _, a := range data {
-		maps.Copy(antinodeMap, a.mapAntinodes())
+		for pos, isFirst := range a.mapAntinodes() {
+			if isFirst {
+				firstHarmonic[pos] = struct{}{}
+			}
+
+			antinodeMap[pos] = struct{}{}
+		}
 	}
 
-	P1 := len(antinodeMap)
-	P2 := 0
+	P1 := len(firstHarmonic)
+	P2 := len(antinodeMap)
 
 	fmt.Printf("P1: %d\n", P1)
 	fmt.Printf("P2: %d\n", P2)
