c79493ecff1f4896bd00e5f8fc7dc9a048b6d26f
Pranav Taysheti <pranavtaysheti@gmail.com>
2024-12-10 14:44:19 +0530

    more OOP

--- 2023/19/python/main.py
+++ 2023/19/python/main.py
@@ -1,52 +1,79 @@
 import fileinput
-from collections.abc import Generator
+from copy import copy
 from fileinput import FileInput
 from itertools import takewhile
-from typing import NamedTuple
+from typing import Literal, NamedTuple, Self
 
-
-class Condition(NamedTuple):
-    category: str
-    operator: str
-    limit: int
+type Action = str | bool
 
 
 class Part(dict[str, int]):
     def score(self) -> int:
         return sum(self.values())
 
+    def check(self) -> bool:
+        curr = workflows["in"]
+        while not isinstance(action := curr.run(self), bool):
+            curr = workflows[action]
+
+        return action
+
+
+class Condition(NamedTuple):
+    category: str
+    operator: Literal["<"] | Literal[">"]
+    limit: int
+
+    def test(self, part: Part) -> bool:
+        if (self.operator == ">" and part[self.category] > self.limit) or (
+            self.operator == "<" and part[self.category] < self.limit
+        ):
+            return True
+
+        return False
+
 
 class Rule(NamedTuple):
     condition: Condition | None
-    action: bool | str
+    action: Action
 
-    def test(self, part: Part) -> bool | str | None:
-        if (
-            (c := self.condition) is None
-            or (c.operator == ">" and part[c.category] > c.limit)
-            or (c.operator == "<" and part[c.category] < c.limit)
-        ):
+    def test(self, part) -> Action | None:
+        if self.condition is None or self.condition.test(part):
             return self.action
 
 
-workflows: dict[str, list[Rule]] = {}
-parts: list[Part] = []
+class Range(NamedTuple):
+    low: int
+    high: int
 
 
-def solve() -> Generator[Part]:
-    def run_workflow(name: str, part: Part) -> bool:
-        for rule in workflows[name]:
-            if (res := rule.test(part)) is None:
-                continue
+class PartRange(dict[str, Range]):
+    def split(self, condition: Condition) -> tuple[Self, Self]:
+        accept, reject = copy(self), copy(self)
 
-            if isinstance(res, bool):
+        o_range = self[condition.category]
+        match condition.operator:
+            case "<":
+                accept[condition.category] = Range(o_range.low, condition.limit - 1)
+                reject[condition.category] = Range(condition.limit, o_range.high)
+            case ">":
+                accept[condition.category] = Range(condition.limit + 1, o_range.high)
+                reject[condition.category] = Range(o_range.low, condition.limit)
+
+        return accept, reject
+
+
+class WorkFlow(list[Rule]):
+    def run(self, part: Part) -> Action:
+        for rule in self:
+            if (res := rule.test(part)) is not None:
                 return res
 
-            return run_workflow(res, part)
+        raise AssertionError("Non redirecting or terminating workflow")
 
-        raise AssertionError("non-terminating workflow")
 
-    return (part for part in parts if run_workflow("in", part))
+workflows: dict[str, WorkFlow] = {}
+parts: list[Part] = []
 
 
 def parse_input(file: FileInput[str]):
@@ -61,10 +88,10 @@ def parse_input(file: FileInput[str]):
                     return action
 
         def parse_condition(condition: str) -> Condition:
-            if any((pos := condition.find(operator := o)) >= 0 for o in ["<", ">"]):
+            if any((pos := condition.find(operator := o)) != -1 for o in ("<", ">")):
                 return Condition(condition[:pos], operator, int(condition[pos + 1 :]))
 
-            raise AssertionError
+            raise AssertionError(f"unknown operator: {operator}")
 
         def parse_rule(rule: str) -> Rule:
             if (index := rule.find(":")) == -1:
@@ -74,9 +101,9 @@ def parse_input(file: FileInput[str]):
             return Rule(parse_condition(condition), parse_action(action))
 
         name = line[: line.index("{")]
-        workflows[name] = [
+        workflows[name] = WorkFlow(
             parse_rule(rule) for rule in line[len(name) + 1 : -2].split(",")
-        ]
+        )
 
     def parse_part(line: str):
         part = Part()
@@ -96,7 +123,7 @@ def parse_input(file: FileInput[str]):
 
 parse_input(fileinput.input(encoding="utf-8"))
 
-P1 = sum(p.score() for p in solve())
+P1 = sum(p.score() for p in parts if p.check())
 P2 = 0
 
 print(f"P1: {P1}")
