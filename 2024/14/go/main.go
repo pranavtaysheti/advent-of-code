edc9505a120287d21b81d6f6756229a9ae58cfb5
Pranav Taysheti <pranavtaysheti@gmail.com>
2024-12-16 20:34:35 +0530

    P1

--- 2024/14/go/main.go
+++ 2024/14/go/main.go
@@ -4,20 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"strconv"
 	"strings"
 )
-
-var height int
-var width int
 
 type robot struct {
 	position [2]int
 	velocity [2]int
 }
 
-func (r robot) newPosition(time int) (res [2]int) {
+func (r robot) newPosition(time int, floorDimensions [2]int) (res [2]int) {
+	height, width := floorDimensions[0], floorDimensions[1]
+
 	correct := func(v, m int) int {
 		v = v % m
 		if v < 0 {
@@ -33,14 +33,16 @@ func (r robot) newPosition(time int) (res [2]int) {
 	return
 }
 
-type floor []robot
+type floor struct {
+	robots     map[robot]struct{}
+	dimensions [2]int
+}
 
 func (f floor) score(time int) int {
+	height, width := f.dimensions[0], f.dimensions[1]
+
 	q1, q2, q3, q4 := 0, 0, 0, 0
-
-	for _, r := range f {
-		newPos := r.newPosition(time)
-
+	for newPos := range f.newPositions(time) {
 		switch {
 		case newPos[0] < height/2 && newPos[1] > width/2:
 			q1++
@@ -54,11 +56,44 @@ func (f floor) score(time int) int {
 	}
 
 	fmt.Println(q1, q2, q3, q4)
-
 	return q1 * q2 * q3 * q4
 }
 
-var data = floor{}
+func (f floor) newPositions(time int) iter.Seq[[2]int] {
+	return func(yield func([2]int) bool) {
+		for r := range f.robots {
+			if !yield(r.newPosition(time, f.dimensions)) {
+				break
+			}
+		}
+	}
+}
+
+// func (f floor) simulate() iter.Seq2[int, map[robot]struct{}] {
+// 	return func(yield func(int, map[robot]struct{}) bool) {
+// 		for time, state := 0, maps.Clone(f.robots); ; time++ {
+// 			for r := range state {
+// 				r.position = r.newPosition(1, f.dimensions)
+// 			}
+
+// 			if !yield(time, state) {
+// 				break
+// 			}
+// 		}
+// 	}
+// }
+
+// func (f floor) simulateScore() int {
+// 	for t, newF := range f.simulate() {
+// 		if t == 100 {
+// 			return newF.score(0)
+// 		}
+// 	}
+
+// 	panic("this shouldnt happen")
+// }
+
+var data = floor{robots: map[robot]struct{}{}}
 
 func parse(r io.Reader) {
 	scanner := bufio.NewScanner(r)
@@ -75,17 +110,15 @@ func parse(r io.Reader) {
 		velocityX, _ := strconv.Atoi(velocityNums[0])
 		velocityY, _ := strconv.Atoi(velocityNums[1])
 
-		data = append(data, robot{
+		data.robots[robot{
 			[2]int{posY, posX},
-			[2]int{velocityY, velocityX}})
+			[2]int{velocityY, velocityX}}] = struct{}{}
 	}
 }
 
 func main() {
 	parse(os.Stdin)
-
-	height = 103
-	width = 101
+	data.dimensions = [2]int{103, 101}
 
 	P1 := data.score(100)
 	P2 := 0
