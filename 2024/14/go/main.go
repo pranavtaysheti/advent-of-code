70bb05054d21c9c308ab2297ad101345f2b9bda7
Pranav Taysheti <pranavtaysheti@gmail.com>
2024-12-16 22:56:19 +0530

    P2

--- 2024/14/go/main.go
+++ 2024/14/go/main.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 	"iter"
+	"math"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -39,24 +41,7 @@ type floor struct {
 }
 
 func (f floor) score(time int) int {
-	height, width := f.dimensions[0], f.dimensions[1]
-
-	q1, q2, q3, q4 := 0, 0, 0, 0
-	for newPos := range f.newPositions(time) {
-		switch {
-		case newPos[0] < height/2 && newPos[1] > width/2:
-			q1++
-		case newPos[0] < height/2 && newPos[1] < width/2:
-			q2++
-		case newPos[0] > height/2 && newPos[1] < width/2:
-			q3++
-		case newPos[0] > height/2 && newPos[1] > width/2:
-			q4++
-		}
-	}
-
-	fmt.Println(q1, q2, q3, q4)
-	return q1 * q2 * q3 * q4
+	return state{&f, slices.Collect(f.newPositions(time))}.score()
 }
 
 func (f floor) newPositions(time int) iter.Seq[[2]int] {
@@ -69,29 +54,83 @@ func (f floor) newPositions(time int) iter.Seq[[2]int] {
 	}
 }
 
-// func (f floor) simulate() iter.Seq2[int, map[robot]struct{}] {
-// 	return func(yield func(int, map[robot]struct{}) bool) {
-// 		for time, state := 0, maps.Clone(f.robots); ; time++ {
-// 			for r := range state {
-// 				r.position = r.newPosition(1, f.dimensions)
-// 			}
+func (f floor) simulate() iter.Seq2[int, [][2]int] {
+	return func(yield func(int, [][2]int) bool) {
+		for time := range math.MaxInt16 {
+			if !yield(time, slices.Collect(f.newPositions(time))) {
+				break
+			}
+		}
+	}
+}
 
-// 			if !yield(time, state) {
-// 				break
-// 			}
-// 		}
-// 	}
-// }
+func (f floor) print(time int) {
+	region := make([][]rune, f.dimensions[0])
+	for i := range f.dimensions[0] {
+		region[i] = make([]rune, f.dimensions[1])
 
-// func (f floor) simulateScore() int {
-// 	for t, newF := range f.simulate() {
-// 		if t == 100 {
-// 			return newF.score(0)
-// 		}
-// 	}
+		for j := range f.dimensions[1] {
+			region[i][j] = ' '
+		}
+	}
 
-// 	panic("this shouldnt happen")
-// }
+	for pos := range f.newPositions(time) {
+		region[pos[0]][pos[1]] = '#'
+	}
+
+	for _, line := range region {
+		fmt.Println(string(line))
+	}
+
+}
+
+type state struct {
+	floor     *floor
+	positions [][2]int
+}
+
+func (s state) score() int {
+	height, width := s.floor.dimensions[0], s.floor.dimensions[1]
+
+	q1, q2, q3, q4 := 0, 0, 0, 0
+	for _, newPos := range s.positions {
+
+		switch {
+		case newPos[0] < height/2 && newPos[1] > width/2:
+			q1++
+		case newPos[0] < height/2 && newPos[1] < width/2:
+			q2++
+		case newPos[0] > height/2 && newPos[1] < width/2:
+			q3++
+		case newPos[0] > height/2 && newPos[1] > width/2:
+			q4++
+		}
+	}
+
+	return q1 * q2 * q3 * q4
+}
+
+func (s state) standardDeviation() [2]float64 {
+	calc := func(index int) float64 {
+		var sum float64
+		for _, p := range s.positions {
+			sum += float64(p[index])
+		}
+
+		mean := sum / float64(len(s.positions))
+
+		var numerator float64
+		for _, p := range s.positions {
+			numerator += math.Pow((float64(p[index]) - mean), 2)
+		}
+
+		return numerator / float64(len(s.positions))
+
+	}
+
+	return [2]float64{calc(0), calc(1)}
+
+}
 
 var data = floor{robots: map[robot]struct{}{}}
 
@@ -121,8 +160,18 @@ func main() {
 	data.dimensions = [2]int{103, 101}
 
 	P1 := data.score(100)
-	P2 := 0
+
+	var P2 int
+	for time, positions := range data.simulate() {
+		v := state{&data, positions}.standardDeviation()
+		if v[0] < 450 && v[1] < 450 {
+			P2 = time
+			break
+		}
+	}
 
 	fmt.Printf("P1: %d\n", P1)
 	fmt.Printf("P2: %d\n", P2)
+
+	data.print(P2)
 }
