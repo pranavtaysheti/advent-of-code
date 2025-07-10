1e28d3c0f766e6c13fd81364c96aedc0a72a1f2b
Pranav Taysheti <pranavtaysheti@gmail.com>
2025-07-10 12:03:31 +0000

    P1 & P2 - method 2

--- 2024/20/go/main.go
+++ 2024/20/go/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"iter"
 	"math"
 	"os"
 	"strings"
@@ -13,6 +14,18 @@ const MIN_TIME = 100
 
 type vector [2]int
 
+func (v vector) distance() int {
+	return max(-v[0], v[0]) + max(-v[1], v[1])
+}
+
+func (v vector) add(a vector) vector {
+	return [2]int{v[0] + a[0], v[1] + a[1]}
+}
+
+func (v vector) substract(s vector) vector {
+	return [2]int{v[0] - s[0], v[1] - s[1]}
+}
+
 var directions = [4]vector{
 	{-1, 0},
 	{0, +1},
@@ -20,17 +33,11 @@ var directions = [4]vector{
 	{0, -1},
 }
 
-func (v vector) addTo(pos [2]int) [2]int {
-	row, col := pos[0], pos[1]
-	c_row, c_col := v[0], v[1]
-
-	return [2]int{row + c_row, col + c_col}
-}
-
 type raceTrack struct {
 	start [2]int
 	end   [2]int
 	track [][]int
+	path  []vector
 }
 
 type cheat struct {
@@ -38,52 +45,66 @@ type cheat struct {
 	end   [2]int
 }
 
-func (r raceTrack) prime() {
+func (r *raceTrack) prime() {
+	r.path = append(r.path, r.start)
+
 	for i, c := 0, r.start; c != r.end; i++ {
 		for _, d := range directions {
-			n_pos := d.addTo(c)
+			n_pos := d.add(c)
 			n_row, n_col := n_pos[0], n_pos[1]
 
 			if r.track[n_row][n_col] == 0 {
 				r.track[n_row][n_col] = i + 1
 				c = [2]int{n_row, n_col}
+				r.path = append(r.path, c)
 				break
 			}
 		}
 	}
 }
 
-func (r raceTrack) findCheats() map[int]int {
-	res := map[int]int{}
-
-	for row, line := range r.track[1 : len(r.track)-1] {
-		row = row + 1
-		for col, e := range line[1 : len(line)-1] {
-			col = col + 1
-			if e < math.MaxInt {
-				continue
-			}
-
-			for i, d := range directions[:2] {
-				s1_pos := d.addTo([2]int{row, col})
-				s1_row, s1_col := s1_pos[0], s1_pos[1]
-
-				s2_pos := directions[i+2].addTo([2]int{row, col})
-				s2_row, s2_col := s2_pos[0], s2_pos[1]
-
-				s1 := r.track[s1_row][s1_col]
-				s2 := r.track[s2_row][s2_col]
-
-				if s1 < math.MaxInt && s2 < math.MaxInt {
-					diff := max(s1, s2) - min(s1, s2)
-					if v, ok := res[diff-2]; !ok {
-						res[diff-2] = 1
-					} else {
-						res[diff-2] = v + 1
-					}
+func combinations[T any](l []T) iter.Seq[[2]T] {
+	return func(yield func([2]T) bool) {
+		for i := range len(l) - 1 {
+			for j := range len(l) - 1 - i {
+				if !yield([2]T{l[i], l[i+j+1]}) {
+					return
 				}
 			}
 		}
+	}
+}
+func (r raceTrack) findCheats2(time int) map[int]int {
+	res := map[int]int{}
+
+	i := 0
+	for v := range combinations(r.path) {
+		v0_steps, v1_steps := r.track[v[0][0]][v[0][1]], r.track[v[1][0]][v[1][1]]
+
+		var dist = v[0].substract(v[1]).distance()
+		if dist < 0 {
+			dist = -dist
+		}
+
+		var diff = v0_steps - v1_steps
+		if diff < 0 {
+			diff = -diff
+		}
+
+		if dist > time || diff == dist {
+			continue
+		}
+
+		time_saved := diff - dist
+
+		v, ok := res[time_saved]
+		if !ok {
+			res[time_saved] = 1
+		} else {
+			res[time_saved] = v + 1
+		}
+
+		i++
 	}
 
 	return res
@@ -117,19 +138,21 @@ func parse(r io.Reader) (m raceTrack) {
 	return
 }
 
+func totalCheats(d raceTrack, max_time int) (res int) {
+	for time_saved, q := range d.findCheats2(max_time) {
+		if time_saved >= MIN_TIME {
+			res += q
+		}
+	}
+
+	return
+}
 func main() {
 	data := parse(os.Stdin)
 	data.prime()
 
-	sum := 0
-	for time, q := range data.findCheats() {
-		if time >= 100 {
-			sum += q
-		}
-	}
-
-	P1 := sum
-	P2 := 0
+	P1 := totalCheats(data, 2)
+	P2 := totalCheats(data, 20)
 
 	fmt.Printf("P1: %d\n", P1)
 	fmt.Printf("P2: %d\n", P2)
