1dd8d0c96337a195fba043ee6712f789f35811c1
Pranav Taysheti <pranavtaysheti@gmail.com>
2025-10-26 18:47:15 +0000

    P1

--- 2024/25/go/main.go
+++ 2024/25/go/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"slices"
 )
 
 const patternLen = 7
@@ -23,8 +24,24 @@ func (p pattern) heightMap(spec patternType) []int {
 	res := make([]int, patternWidth)
 	switch spec {
 	case ptLock:
-
+		for i, row := range p {
+			for j, c := range row {
+				if c == '#' {
+					res[j] = i + 1
+				}
+			}
+		}
 	case ptKey:
+		var p_rev = pattern{}
+		copy(p_rev[:], p[:])
+		slices.Reverse(p_rev[:])
+		for i, row := range p_rev {
+			for j, c := range row {
+				if c == '#' {
+					res[j] = i + 1
+				}
+			}
+		}
 	}
 
 	return res
@@ -40,36 +57,68 @@ var data = schematics{
 	keys:  []pattern{},
 }
 
-func (s schematics) solve() {
+func (s schematics) solve() (res int) {
+	for _, l := range data.locks {
+		hm_l := l.heightMap(ptLock)
 
+	key:
+		for _, k := range data.keys {
+			hm_k := k.heightMap(ptKey)
+			for i, h := range hm_k {
+				if hm_l[i]+h > patternLen {
+					continue key
+				}
+			}
+
+			res++
+		}
+	}
+
+	return
 }
 
 func parse(r io.Reader) {
+	checkFilled := func(r [patternWidth]rune) bool {
+		for _, c := range r {
+			if c != '#' {
+				return false
+			}
+		}
+
+		return true
+	}
+
 	scanner := bufio.NewScanner(r)
-	curr := pattern{}
+
+	var curr pattern
 	for i := 0; scanner.Scan(); i++ {
 		line := scanner.Text()
 
-		if i%(patternLen+1) == patternLen {
-			switch {
-			case curr[0] == [patternWidth]rune{'#', '#', '#', '#', '#'}:
-				data.locks = append(data.locks, curr)
-			case curr[patternLen-1] == [patternWidth]rune{'#', '#', '#', '#', '#'}:
-				data.keys = append(data.keys, curr)
-			}
-
+		if i%(patternLen+1) == 0 {
 			curr = pattern{}
-			continue
 		}
 
-		curr[i%(patternLen+1)] = [5]rune([]rune(line))
+		if i%(patternLen+1) != patternLen {
+			curr[i%(patternLen+1)] = [5]rune([]rune(line))
+		}
+
+		if i%(patternLen+1) == patternLen-1 {
+			switch {
+			case checkFilled(curr[0]):
+				data.locks = append(data.locks, curr)
+			case checkFilled(curr[patternLen-1]):
+				data.keys = append(data.keys, curr)
+			}
+		}
 	}
+
+	fmt.Println(len(data.locks) + len(data.keys))
 }
 
 func main() {
 	parse(os.Stdin)
 
-	P1 := 0
+	P1 := data.solve()
 	P2 := 0
 
 	fmt.Printf("P1: %d\n", P1)
