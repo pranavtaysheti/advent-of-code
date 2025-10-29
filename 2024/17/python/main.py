0fda566b6acc29b4bea075f1211f9b106fd1ad2f
Pranav Taysheti <pranavtaysheti@gmail.com>
2025-10-29 21:15:30 +0000

    P1 & P2

--- 2024/17/python/main.py
+++ 2024/17/python/main.py
@@ -1,6 +1,7 @@
 import fileinput
 from collections.abc import Callable
 from enum import Enum, auto
+from itertools import product
 from operator import and_, rshift, xor
 from typing import NamedTuple
 
@@ -11,11 +12,11 @@ class OperandType(Enum):
     RegC = auto()
 
 
-class Instruction_W0(NamedTuple):
+class InstructionW0(NamedTuple):
     name: str
 
 
-class Instruction_W2(NamedTuple):
+class InstructionW2(NamedTuple):
     name: str
     operand_type: OperandType
     operator: Callable[[int, int], int]
@@ -23,42 +24,41 @@ class Instruction_W2(NamedTuple):
 
 
 class VirtualMachine:
-    type Instruction = Instruction_W0 | Instruction_W2
+    type Instruction = InstructionW0 | InstructionW2
 
     instructions: list[Instruction] = [
-        Instruction_W2("adv", OperandType.Combo, rshift, 4),
-        Instruction_W2("bxl", OperandType.Literal, xor, 5),
-        Instruction_W2("bst", OperandType.Combo, and_, 7),
-        Instruction_W0("jnz"),
-        Instruction_W2("bxc", OperandType.RegC, xor, 5),  # second operand is c
-        Instruction_W2("out", OperandType.Combo, and_, 7),
-        Instruction_W2("bdv", OperandType.Combo, rshift, 4),
-        Instruction_W2("cdv", OperandType.Combo, rshift, 4),
+        InstructionW2("adv", OperandType.Combo, rshift, 4),
+        InstructionW2("bxl", OperandType.Literal, xor, 5),
+        InstructionW2("bst", OperandType.Combo, and_, 7),
+        InstructionW0("jnz"),
+        InstructionW2("bxc", OperandType.RegC, xor, 5),  # second operand is c
+        InstructionW2("out", OperandType.Combo, and_, 7),
+        InstructionW2("bdv", OperandType.Combo, rshift, 4),
+        InstructionW2("cdv", OperandType.Combo, rshift, 4),
     ]
 
-    def __init__(self) -> None:
-        self.cursor = 0
-        self.registers = [0, 1, 2, 3, *registers, 7]
+    def __init__(self, program: list[int], regs: tuple[int, int, int]) -> None:
+        self.program = program
+        self.registers = [0, 1, 2, 3, *regs, 7]
 
-    def execute(self) -> int | None:
-        ins = self.instructions[program[self.cursor]]
-        res = None
+    def exec_ins(self, cursor: int) -> tuple[int | None, int]:
+        ins = self.instructions[program[cursor]]
+        out: int | None = None
 
         def write(sol: int):
+            nonlocal out
             if ins.name[0] == "o":
-                nonlocal res
-                res = sol
+                out = sol
 
             else:
                 self.registers[ord(ins.name[0]) - 93] = sol
 
-        operand_2 = int(program[self.cursor + 1])
+        operand_2 = int(program[cursor + 1])
         if ins.name == "jnz":
             if self.registers[4] != 0:
-                self.cursor = operand_2
-                return
+                return (out, operand_2)
         else:
-            assert not isinstance(ins, Instruction_W0)
+            assert not isinstance(ins, InstructionW0)
             match ins.operand_type:
                 case OperandType.Combo:
                     operand_2 = self.registers[operand_2]
@@ -66,34 +66,66 @@ class VirtualMachine:
                 case OperandType.RegC:
                     operand_2 = self.registers[6]
 
-            if isinstance(ins, Instruction_W2):
+            if isinstance(ins, InstructionW2):
                 operand_1 = self.registers[ins.first_operand]
                 write(ins.operator(operand_1, operand_2))
             else:
                 write(ins.operator(operand_2))
 
-        self.cursor += 2
+        return out, cursor + 2
+
+    def run_out(self) -> int:
+        cursor = 0
+        res = None
+        while res is None:
+            out, n_ins = self.exec_ins(cursor)
+            if out is not None:
+                res = out
+
+            cursor = n_ins
+
         return res
 
-    def run_program(self) -> bytearray:
-        output = bytearray()
-        while self.cursor < len(program):
-            if (out := self.execute()) is not None:
+    def run_program(self) -> list[int]:
+        cursor = 0
+        output: list[int] = []
+        while cursor < len(program):
+            out, n_ins = self.exec_ins(cursor)
+            if out is not None:
                 output.append(out)
+
+            cursor = n_ins
 
         return output
 
 
+def find_inp(program: list[int], out_req: list[int]) -> list[int]:
+    res: list[int] = [0]
+    for o in reversed(out_req):
+        n_res: list[int] = []
+        for app, num in product(range(8), res):
+            n_num = (num << 3) + app
+            vm = VirtualMachine(program, (n_num, 0, 0))
+            if vm.run_out() == o:
+                n_res.append(n_num)
+
+        res = n_res
+
+    return res
+
+
 with fileinput.input() as input_file:
-    registers: list[int] = [
+    regs: tuple[int, int, int] = (
         int(input_file.readline().split()[2]),
         int(input_file.readline().split()[2]),
         int(input_file.readline().split()[2]),
-    ]
+    )
 
     input_file.readline()
 
     program = [int(s) for s in input_file.readline().split()[1].split(",")]
 
-    print(f"P1: {",".join(str(i) for i in VirtualMachine().run_program())}")
-    print(f"P2: {0}")
+    vm = VirtualMachine(program, regs)
+    print(f"P1: {",".join(str(i) for i in vm.run_program())}")
+
+    print(f"P2: {min(find_inp(program, program))}")
