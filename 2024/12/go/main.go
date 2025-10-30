9c6dbc042a99116caea9be4c7ae73fb771dd3e98
Pranav Taysheti <pranavtaysheti@gmail.com>
2025-10-30 15:39:48 +0000

    faster P1

--- 2024/12/go/main.go
+++ 2024/12/go/main.go
@@ -4,22 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"math/bits"
 	"os"
 )
 
+type fenceType uint
+
+const (
+	ftVerLeft fenceType = 1 << iota
+	ftVerRight
+	ftHorTop
+	ftHorBottom
+)
+
+func (ft fenceType) count() (count int) {
+	return bits.OnesCount(uint(ft))
+}
+
+var vectors = map[fenceType][2]int{
+	ftVerLeft:   {0, -1},
+	ftVerRight:  {0, +1},
+	ftHorTop:    {-1, 0},
+	ftHorBottom: {+1, 0},
+}
+
+type region map[[2]int]fenceType
+
+func (r region) fencingCost() (area int, perimeter int) {
+	for _, fenceType := range r {
+		area++
+		perimeter += fenceType.count()
+	}
+
+	return
+}
+
 type farm [][]rune
 
-var data = farm([][]rune{})
-
 func (f farm) solve() (cost int) {
-	seen := map[[2]int]struct{}{}
+	seen := make([][]bool, len(f))
+	for i := range len(seen) {
+		seen[i] = make([]bool, len(f[i]))
+	}
 
-	for i, line := range data {
+	for i, line := range f {
 		for j := range line {
-			if _, ok := seen[[2]int{i, j}]; !ok {
+			if seen[i][j] == false {
 				region := f.fill([2]int{i, j})
 				for pos := range region {
-					seen[pos] = struct{}{}
+					seen[pos[0]][pos[1]] = true
 				}
 
 				area, perimeter := region.fencingCost()
@@ -31,65 +64,65 @@ func (f farm) solve() (cost int) {
 	return
 }
 
-func (f farm) fill(pos [2]int) (r region) {
-	r = make(region)
-	plant := f[pos[0]][pos[1]]
-	vectors := [4][2]int{{-1, 0}, {0, +1}, {+1, 0}, {0, -1}}
+func (f farm) fill(rootPos [2]int) (reg region) {
+	reg = make(region)
+	root := f[rootPos[0]][rootPos[1]]
+	nextPos := [][2]int{rootPos}
+	for len(nextPos) > 0 {
+		currPos := nextPos[len(nextPos)-1]
+		nextPos = nextPos[:len(nextPos)-1]
 
-	poi := [][2]int{pos}
-	for len(poi) > 0 {
-		currPos := poi[len(poi)-1]
-		r[currPos] = 0
-		poi = poi[:len(poi)-1]
-
-		for _, v := range vectors {
-			nPos := [2]int{currPos[0] + v[0], currPos[1] + v[1]}
-			if nPos[0] < 0 || nPos[0] >= len(f) ||
-				nPos[1] < 0 || nPos[1] >= len(f[0]) {
-				r[currPos]++
+		for ft, v := range vectors {
+			np := [2]int{currPos[0] + v[0], currPos[1] + v[1]}
+			if np[0] < 0 {
+				reg[currPos] |= ftHorTop
+				continue
+			}
+			if np[0] >= len(f) {
+				reg[currPos] |= ftHorBottom
+				continue
+			}
+			if np[1] < 0 {
+				reg[currPos] |= ftVerLeft
+				continue
+			}
+			if np[1] >= len(f[0]) {
+				reg[currPos] |= ftVerRight
 				continue
 			}
 
-			if f[nPos[0]][nPos[1]] == plant {
-				if _, ok := r[nPos]; !ok {
-					poi = append(poi, nPos)
+			if f[np[0]][np[1]] == root {
+				if _, ok := reg[np]; !ok {
+					nextPos = append(nextPos, np)
+					reg[np] = 0
 				}
 			} else {
-				r[currPos]++
+				reg[currPos] |= ft
 			}
 		}
 	}
 
-	return
+	return reg
 }
 
-type region map[[2]int]int
-
-func (r region) fencingCost() (area int, perimeter int) {
-	for _, ends := range r {
-		area++
-		perimeter += ends
-	}
-
-	return
-}
-
-func parse(r io.Reader) {
+func parse(r io.Reader) (res [][]rune) {
 	scanner := bufio.NewScanner(r)
-
 	for i := 0; scanner.Scan(); i++ {
-		data = append(data, []rune{})
+		res = append(res, []rune{})
 
 		for _, r := range scanner.Text() {
-			data[i] = append(data[i], r)
+			res[i] = append(res[i], r)
 		}
 	}
+
+	return res
 }
 
 func main() {
-	parse(os.Stdin)
+	data := parse(os.Stdin)
+	farm := farm(data)
 
-	P1 := data.solve()
+	P1 := farm.solve()
 	P2 := 0
 
 	fmt.Printf("P1: %d\n", P1)
