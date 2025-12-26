a67cb7f7121e9409b9d64b48a68eae4bb62c8684
Pranav Taysheti <pranavtaysheti@gmail.com>
2025-12-26 11:55:27 +0000

    P2

--- 2025/07/python/main.py
+++ 2025/07/python/main.py
@@ -1,43 +1,57 @@
 import fileinput
-from collections import UserList
-from typing import Iterable
+from functools import cache
+from typing import Iterable, Literal
+
+type Cell = Literal["^"] | int
 
 
-class Room(UserList[list[str]]):
+class Room:
     def __init__(self, inp: Iterable[str]):
         self.curr = 0
-        data = [[c for c in l] for l in inp]
-        for i, c in enumerate(data[0]):
-            if c == "S":
-                data[0][i] = "|"
-                break
 
-        super().__init__(data)
+        def rep(c) -> Cell:
+            match c:
+                case "^":
+                    return c
+                case ".":
+                    return 0
+                case "S":
+                    return 1
+
+            raise AssertionError("Input Data has errors")
+
+        data: list[list[Cell]] = [[rep(c) for c in l] for l in inp]
+        self._data = data
 
     def process_line(self):
         splits: int = 0
         for i, (prev, curr) in enumerate(
-            zip(self.data[self.curr], self.data[self.curr + 1])
+            zip(self._data[self.curr], self._data[self.curr + 1])
         ):
-            if prev != "|":
+            if not isinstance(prev, int):
                 continue
 
             if curr == "^":
                 if i > 0:
-                    self.data[self.curr + 1][i + 1] = "|"
-                if i < len(self.data[self.curr + 1]) - 1:
-                    self.data[self.curr + 1][i - 1] = "|"
+                    self._data[self.curr + 1][i + 1] += prev
+                if i < len(self._data[self.curr + 1]) - 1:
+                    self._data[self.curr + 1][i - 1] += prev
 
                 splits += 1
 
             else:
-                self.data[self.curr + 1][i] = "|"
+                self._data[self.curr + 1][i] += prev
 
         self.curr += 1
         return splits
 
+    @cache
     def process(self) -> list[int]:
-        return [self.process_line() for _ in range(len(self.data) - 1)]
+        return [self.process_line() for _ in range(len(self._data) - 1)]
+
+    def count_timelines(self):
+        self.process()
+        return sum(c for c in self._data[-1])
 
 
 def parse() -> Room:
@@ -47,3 +61,4 @@ def parse() -> Room:
 if __name__ == "__main__":
     data = parse()
     print(f"P1: {sum(i for i in data.process())}")
+    print(f"P2: {data.count_timelines()}")
